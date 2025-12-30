ce196dce596f9470ff302cd044ec520cad559eab
vivekkumar1155 <vivek.kumar@barcodeindia.com>
2025-12-30 10:37:32 +0530

    SN generation: allow barcode to generate serials with any other workorders

--- DIXON/Main.Master.designer.cs
+++ DIXON/Main.Master.designer.cs
@@ -141,6 +141,15 @@ namespace DIXON
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl listPacking;
 
         /// <summary>
+        /// lstQuality control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl lstQuality;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
